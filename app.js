36d21e83605ca9dc4c2acbe728617faf07c86e07
Sayannaskar1 <144678374+Sayannaskar1@users.noreply.github.com>
2025-08-08 01:05:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-const express=require("express");
-const app=express();
-const path=require("path");
-const http=require("http");
-const socketio=require("socket.io");
-const server=http.createServer(app);
-const io=socketio(server);
-app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname,"public")));
-io.on("connection",function(socket){
-    socket.on("send-location", function(data){
-        io.emit("receive-location",{ id: socket.id, ...data });
+const express = require("express");
+const app = express();
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set the views directory
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function(socket) {
+    socket.on("send-location", function(data) {
+        io.emit("receive-location", { id: socket.id, ...data });
     });
- socket.on("disconnect",function(){
-    io.emit("user-disconnected",socket.id);
- });
+    socket.on("disconnect", function() {
+        io.emit("user-disconnected", socket.id);
+    });
 });
-app.get("/",function(req,res){
+
+app.get("/", function(req, res) {
     res.render("index");
 });
-server.listen(3000);
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
